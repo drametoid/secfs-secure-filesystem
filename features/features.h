9b0f881fb215d95098dcd29184d62d0dfa67398b
raghukapur9 <64493087+raghukapur9@users.noreply.github.com>
2024-02-14 23:05:25 -0800

    added basic function structure

--- features/features.h
+++ features/features.h
@@ -6,4 +6,15 @@
 #ifndef FEATURES_H
 #define FEATURES_H
 
+#include <iostream>
+
+int userFeatures() {
+  std::cout << "++++++++++++++++++++++++" << std::endl;
+  std::cout << "++| WELCOME TO EFS! |++" << std::endl;
+  std::cout << "++++++++++++++++++++++++" << std::endl;
+  std::cout << "\nEFS Commands Available: \n" << std::endl;
+
+  return 1;
+}
+
 #endif // FEATURES_H
