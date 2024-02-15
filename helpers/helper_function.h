5c14807acae859df15407f2d2dc476d0c18be2e9
Shubham Bhatia <39002688+drametoid@users.noreply.github.com>
2024-02-15 01:36:46 -0800

    adds helper function for createDirectory

--- helpers/helper_function.h
+++ helpers/helper_function.h
@@ -5,4 +5,26 @@
 #ifndef HELPER_FUNCTIONS_H
 #define HELPER_FUNCTIONS_H
 
+#include <filesystem>
+#include <iostream>
+
+namespace fs = std::filesystem;
+
+/// Helper function to create a new directory
+/// \param path Path for creating dir
+bool createDirectory(const fs::path& path) {
+    std::error_code ec;
+    if (fs::create_directory(path, ec)) {
+        return true;
+    } else {
+        if (ec) {
+            std::cerr << "Error creating directory: " << ec.message() << " (" << path << ")" << std::endl;
+            return false;
+        } else {
+            std::cerr << "Directory already exists: " << path << std::endl;
+            return true;
+        }
+    }
+}
+
 #endif // HELPER_FUNCTIONS_H
