0021ff6e88ce5bd8947822e8a8a6f1683f0b1fa4
Kaiyu Dong <dky815@gmail.com>
2024-02-17 14:20:18 -0500

    add encryption funcs def

--- encryption/encryption.h
+++ encryption/encryption.h
@@ -6,4 +6,28 @@
 #ifndef FILESERVER_ENCRYPTION_H
 #define FILESERVER_ENCRYPTION_H
 
+#include <openssl/conf.h>
+#include <openssl/evp.h>
+#include <openssl/err.h>
+#include <openssl/rand.h>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+#define BLOCK_SIZE 16 //bytes
+#define KEY_SIZE 32 //bytes
+#define TAG_SIZE 16 //bytes
+#define IV_SIZE 16 //bytes
+
+class Encryption {
+public:
+    static void encryptFile(const std::string& filePath, const std::string& content, const std::vector<uint8_t>& key);
+    static std::string decryptFile(const std::string& filePath, const std::vector<uint8_t>& key);
+
+private:
+    static void handleErrors(const std::string& message);
+    static void initCipherContext(EVP_CIPHER_CTX*& ctx, const std::vector<uint8_t>& key, const uint8_t* iv, bool encrypt);
+};
+
 #endif // FILESERVER_ENCRYPTION_H
