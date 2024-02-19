7fc64285611db8d4becb7a27d7fe46fa3d6da8b4
Shubham Bhatia <39002688+drametoid@users.noreply.github.com>
2024-02-19 09:35:31 -0800

    updates userFeatures function definition

--- features/features.h
+++ features/features.h
@@ -8,13 +8,28 @@
 
 #include <iostream>
 
-int userFeatures() {
-  std::cout << "++++++++++++++++++++++++" << std::endl;
-  std::cout << "++| WELCOME TO EFS! |++" << std::endl;
-  std::cout << "++++++++++++++++++++++++" << std::endl;
-  std::cout << "\nEFS Commands Available: \n" << std::endl;
+#include "authentication/authentication.h"
 
-  return 1;
+int userFeatures(std::string user_name, UserType user_type, std::vector<uint8_t> key, std::string filesystem_path) {
+    std::cout << "++++++++++++++++++++++++" << std::endl;
+    std::cout << "++| WELCOME TO EFS! |++" << std::endl;
+    std::cout << "++++++++++++++++++++++++" << std::endl;
+    std::cout << "\nEFS Commands Available: \n" << std::endl;
+
+    std::cout << "cd <directory> \n"
+            "pwd \n"
+            "ls  \n"
+            "cat <filename> \n"
+            "share <filename> <username> \n"
+            "mkdir <directory_name> \n"
+            "mkfile <filename> <contents> \n"
+            "exit \n";
+    if (user_type == admin) {
+        // if admin, allow the following command as well
+        std::cout << "adduser <username>" << std::endl;
+        std::cout << "++++++++++++++++++++++++" << std::endl;
+    }
+    return 1;
 }
 
 #endif // FEATURES_H
